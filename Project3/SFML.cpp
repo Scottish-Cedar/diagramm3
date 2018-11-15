d81108d65ec04c8f501cb59db2c8e9cff91e5962
Cedar <fra99er2@gmail.com>
2018-11-15 11:19:38 +0300

    2

--- Project3/SFML.cpp
+++ Project3/SFML.cpp
@@ -4,7 +4,9 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
 	sf::CircleShape shape(200.f);
-	shape.setFillColor(sf::Color::Magenta);
+	shape.setFillColor(sf::Color(123,945,191));
+	shape.setOutlineThickness(-198.f);
+	shape.setOutlineColor(sf::Color(250, 150, 100));
 	while (window.isOpen())
 	{
 		sf::Event event;
