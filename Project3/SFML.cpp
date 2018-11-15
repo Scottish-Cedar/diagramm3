a2455fa5ef9567d860b243da265d39c1b9d47c24
Cedar <fra99er2@gmail.com>
2018-11-15 11:02:51 +0300

    changed color

--- Project3/SFML.cpp
+++ Project3/SFML.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
 	sf::CircleShape shape(200.f);
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Magenta);
 	while (window.isOpen())
 	{
 		sf::Event event;
