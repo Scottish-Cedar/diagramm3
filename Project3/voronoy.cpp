f0cf35775e7d0da1401ede7e738de2c6561ff46d
Scottish-Cedar <fra99er2@gmail.com>
2018-11-30 07:20:20 +0200

    Update voronoy.cpp
    Border line v.0

--- Project3/voronoy.cpp
+++ Project3/voronoy.cpp
@@ -1,33 +1,37 @@
-
 #include <SFML/Graphics.hpp>
 #include <iostream>
 using namespace std;
 
-
-sf::VertexArray spots(int i, int j)
-{
-	sf::VertexArray spot(sf::Points, 1);
-	spot[0].position = sf::Vector2f(i, j);
-	spot[0].color = sf::Color::Red;
-	return spot;
-}
-
 const int N = 10;
-vector<pair<int, int>> v1(N);
-
+vector<pair<int, int>> vsp(N);
+vector<int> valg(N);
 
 int main()
 {
-
+	int j1 = 0;
+	//int j2 = 0;
 	int i = 0;
 	while (i < N)
 	{
-		v1[i] = make_pair(7 * i*i - 43 * i + 205, 5 * i*i - 26 * i + 208);
-		cout << v1[i].first << ' ' << v1[i].second << endl;
+		vsp[i] = make_pair(7 * i*i - 43 * i + 205, 5 * i*i - 26 * i + 208);
+		cout << vsp[i].first << ' ' << vsp[i].second << endl;
 		i++;
 	}
 	i = 0;
-	sf::RenderWindow window(sf::VideoMode(900, 900), "SFML works!");
+	while (i < N)
+	{
+		valg[i] = vsp[i].first;
+		i++;
+	}
+	sort(valg.begin(), valg.end());
+	i = 0;
+	while (i < N)
+	{
+		cout << valg[i] << endl;
+		i++;
+	}
+	i = 0;
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -36,17 +40,37 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		sf::VertexArray spot(sf::Points, N);
+
+		
 		window.clear();
+		
+		sf::VertexArray line(sf::Lines, 2 * N);
 		i = 0;
-		while (i < N)
-		{
-			spot[i].position = sf::Vector2f(v1[i].first, v1[i].second);
-			spot[i].color = sf::Color::Red;
-			i++;
-		}
-		window.draw(spot);
-		window.display();
+	while (i < N )
+	{
+		line[j1].position = sf::Vector2f(valg[i], 0);
+		line[j1 + 1].position = sf::Vector2f(valg[i], 499);
+		line[j1].color = sf::Color::Cyan;
+		line[j1 + 1].color = sf::Color::Cyan;
+		window.draw(line);
+		//system("pause");
+		line[j1].color = sf::Color::Black;
+		line[j1 + 1].color = sf::Color::Black;
+		window.draw(line);
+		if (j1!=18)
+		j1 += 2;
+		i++;
+	}
+	sf::VertexArray spot(sf::Points, N);
+	i = 0;
+	while (i < N)
+	{
+		spot[i].position = sf::Vector2f(vsp[i].first, vsp[i].second);
+		spot[i].color = sf::Color::Red;
+		i++;
+	}
+	window.draw(spot);
+	window.display();
 	}
 	return 0;
 }
